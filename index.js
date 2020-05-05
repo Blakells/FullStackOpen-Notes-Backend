caaeff1bd44c1a2805584760a060b8209fb84da5
Blakells <blake.bailey09@gmail.com>
2020-05-05 14:22:05 -0400

    3c done

--- index.js
+++ index.js
@@ -4,30 +4,26 @@ const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
 
-// body parser for parsing the body when posting new notes to server
+// middleware
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
-let notes = [
-  {
-    id: 1,
-    content: "HTML is ez-pz",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err.message)
+  if (err.name === 'CastError') {
+    return res.status(400).send({error: 'malformatted id'})
   }
-]
+  next(err)
+}
+app.use(errorHandler)
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint)
+/////////////////////////////////////////////////////////////////
+
 // landing page
 app.get('/', (req, res) => {
   res.send('<h1>Hello world!!</h1>')
@@ -41,17 +37,42 @@ app.get('/api/notes', (req, res) => {
 })
 
 // get a single note by ID
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
   Note.findById(req.params.id).then(note => {
-    res.json(note.toJSON())
+    if (note) {
+      res.json(note.toJSON())
+    } else {
+      res.status(404).end()
+    }
+  })
+  .catch(err => {
+    next(err)
   })
 })
 
 // delete a single note by ID
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-  res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+  .then(res => {
+    console.log('note removed successfully')
+    res.status(204).end()
+  })
+  .catch(err => next(err))
+})
+
+//update note importance
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+  const note = {
+    content: body.content,
+    important : body.important
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, {new : true})
+  .then(updatedNote => {
+    res.json(updatedNote.toJSON())
+  })
+  .catch(err => next(err))
 })
 
 // add new notes to the server
@@ -75,6 +96,7 @@ app.post('/api/notes', (req, res) => {
 })
 
 
+//PORT configuration
 const PORT = process.env.PORT
 app.listen(PORT, () =>{
   console.log(`Server running on port ${PORT}`)
