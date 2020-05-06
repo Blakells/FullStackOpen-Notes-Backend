5203085af1f4b4b91096273bb70a8f9cb64d09b1
Blakells <blake.bailey09@gmail.com>
2020-05-06 14:51:29 -0400

    4a

--- index.js
+++ index.js
@@ -1,108 +1,11 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const Note = require('./models/note')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-// middleware
-app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
-
-// const unknownEndpoint = (req, res) => {
-//   res.status(404).send({ error: "unknown endpoint" });
-// };
-// app.use(unknownEndpoint)
-/////////////////////////////////////////////////////////////////
-
-// landing page
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world!!</h1>')
-})
-
-// get all notes within the DB
-app.get('/api/notes', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes.map(note => note.toJSON()))
-  })
-})
-
-// get a single note by ID
-app.get('/api/notes/:id', (req, res, next) => {
-  Note.findById(req.params.id).then(note => {
-    if (note) {
-      res.json(note.toJSON())
-    } else {
-      res.status(404).end()
-    }
-  })
-  .catch(err => {
-    next(err)
-  })
-})
-
-// delete a single note by ID
-app.delete('/api/notes/:id', (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-  .then(res => {
-    console.log('note removed successfully')
-    res.status(204).end()
-  })
-  .catch(err => next(err))
-})
-
-//update note importance
-app.put('/api/notes/:id', (req, res, next) => {
-  const body = req.body
-  const note = {
-    content: body.content,
-    important : body.important
-  }
-
-  Note.findByIdAndUpdate(req.params.id, note, {new : true})
-  .then(updatedNote => {
-    res.json(updatedNote.toJSON())
-  })
-  .catch(err => next(err))
-})
-
-// add new notes to the server
-app.post('/api/notes', (req, res, next) => {
-  const body = req.body
-
-  if (!body.content) {
-    return res.status(400).json({
-      error: 'content missing'
-    })
-  }
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  })
-  note.save().then(savedNote =>savedNote.toJSON())
-  .then( savedAndFormattedNote => {
-    console.log(savedAndFormattedNote)
-    res.json(savedAndFormattedNote)
-  })
-  .catch(err => next(err))
-})
-
-//error handler middleware
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return res.status(400).send({error: 'malformatted id'})
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({error: error.message})
-  }
-  next(error)
-}
-app.use(errorHandler)
-
+const server = http.createServer(app)
 
 //PORT configuration
-const PORT = process.env.PORT
-app.listen(PORT, () =>{
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
