f4316eb2366788814660e121bbfda624683a5a7e
Blakells <blake.bailey09@gmail.com>
2020-05-11 13:28:10 -0400

    4c

--- models/note.js
+++ models/note.js
@@ -1,16 +1,20 @@
+  
 const mongoose = require('mongoose')
-mongoose.set('useFindAndModify', false)
 
 const noteSchema = new mongoose.Schema({
-    content: {
-      type: String,
-      minlength: 5,
-      required: true
-    },
-    date: Date,
-    important: Boolean
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  date: Date,
+  important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
-//format schema correctly
+
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,5 @@
 const Note = require('../models/note')
+const User = require('../models/user')
 
 const initialNotes = [
   {
@@ -6,15 +7,16 @@ const initialNotes = [
     important: false
   },
   {
-    content: "Browser can execute only Javascript",
-    importnat: true
+    content: 'Browser can execute only Javascript',
+    important: true
   }
 ]
 
 const nonExistingId = async () => {
-  const note = new Note({ content: 'willremovethissoon'})
+  const note = new Note({ content: 'willremovethissoon' })
   await note.save()
   await note.remove()
+
   return note._id.toString()
 }
 
@@ -23,8 +25,11 @@ const notesInDb = async () => {
   return notes.map(note => note.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
-  initialNotes,
-  nonExistingId,
-  notesInDb
+  initialNotes, nonExistingId, notesInDb, usersInDb
 }
