222cf33a292a4eee8494bad7346b8cf1cce48d88
Blakells <blake.bailey09@gmail.com>
2020-05-20 08:48:41 -0400

    added cypess

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+mongoose.set('useCreateIndex', true)
 
 logger.info('connecting to', config.MONGODB_URI)
 
